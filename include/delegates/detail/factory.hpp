2aae6c780556e4a0a45c9931e9096d12470df0b2
alexb <alexb@clever.team>
2025-01-08 17:01:55 +0300

    factory updated based on real usage

--- include/delegates/detail/factory.hpp
+++ include/delegates/detail/factory.hpp
@@ -12,7 +12,7 @@ namespace factory {
 
 // raw pointers
 
-template <class TClass, typename TResult, typename... TArgs>
+template <class TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static IDelegate* make_method_delegate(TClass* callee,
                                         TResult (TClass::*method)(TArgs...),
                                         TArgs... args) {
@@ -26,7 +26,7 @@ static IDelegate* make_method_delegate(TClass* callee,
   return new MethodDelegate<TClass, TResult, TArgs...>(callee, method, std::move(params));
 }
 
-template <class TClass, typename TResult, typename... TArgs>
+template <class TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static IDelegate* make_const_method_delegate(const TClass* callee,
                                         TResult (TClass::*method)(TArgs...) const,
                                         TArgs... args) {
@@ -40,7 +40,7 @@ static IDelegate* make_const_method_delegate(const TClass* callee,
   return new ConstMethodDelegate<TClass, TResult, TArgs...>(callee, method, std::move(params));
 }
 
-template <typename TResult, typename... TArgs>
+template <typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static IDelegate* make_function_delegate(TResult f(TArgs...), TArgs... args) {
   return new FunctionalDelegate<TResult, TArgs...>(std::function<TResult(TArgs...)>(f), DelegateArgs<TArgs...>(std::forward<TArgs>(args)...));
 }
@@ -50,7 +50,7 @@ static IDelegate* make_function_delegate(TResult f(TArgs...), DelegateArgs<TArgs
   return new FunctionalDelegate<TResult, TArgs...>(std::function<TResult(TArgs...)>(f), std::move(params));
 }
 
-template <typename TResult, typename... TArgs>
+template <typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static IDelegate* make_function_delegate(std::function<TResult(TArgs...)> func, TArgs... args) {
   return new FunctionalDelegate<TResult, TArgs...>(func, DelegateArgs<TArgs...>(std::forward<TArgs>(args)...));
 }
@@ -67,7 +67,7 @@ static IDelegate* make_lambda_delegate(F && lambda, DelegateArgs<TArgs...> && pa
 
 // class instance pointer is weak_ptr
 
-template <class TClass, typename TResult, typename... TArgs>
+template <class TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static IDelegate* make_method_delegate(std::weak_ptr<TClass> callee,
                                        TResult (TClass::*method)(TArgs...),
                                        TArgs... args) {
@@ -85,7 +85,7 @@ static IDelegate* make_method_delegate(std::weak_ptr<TClass> callee,
 
 // class instance pointer is shared ptr
 
-template <class TClass, typename TResult, typename... TArgs>
+template <class TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static IDelegate* make_method_delegate(std::shared_ptr<TClass> callee,
                                        TResult (TClass::*method)(TArgs...),
                                        TArgs... args) {
@@ -101,7 +101,7 @@ static IDelegate* make_method_delegate(std::shared_ptr<TClass> callee,
     callee, method, std::move(params));
 }
 
-template <class TClass, typename TResult, typename... TArgs>
+template <class TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static IDelegate* make_const_method_delegate(std::shared_ptr<TClass> callee,
                                              TResult(TClass::* method)(TArgs...) const,
                                              TArgs... args) {
@@ -119,7 +119,7 @@ static IDelegate* make_const_method_delegate(std::shared_ptr<TClass> callee,
 
 // shared
 
-template <class TClass, typename TResult, typename... TArgs>
+template <class TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::shared_ptr<IDelegate> make_shared_method_delegate(
   std::shared_ptr<TClass> callee, TResult (TClass::*method)(TArgs...), TArgs... args) {
   return std::make_shared<SharedMethodDelegate<TClass, TResult, TArgs...> >(
@@ -135,7 +135,7 @@ static std::shared_ptr<IDelegate> make_shared_method_delegate(
     callee, method, std::move(params));
 }
 
-template <class TClass, typename TResult, typename... TArgs>
+template <class TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::shared_ptr<IDelegate> make_shared_method_delegate(
   TClass* callee, TResult (TClass::*method)(TArgs...), TArgs... args) {
   return std::make_shared<MethodDelegate<TClass, TResult, TArgs...> >(callee, method, DelegateArgs<TArgs...>(std::forward<TArgs>(args)...));
@@ -147,7 +147,7 @@ static std::shared_ptr<IDelegate> make_shared_method_delegate(
   return std::make_shared<MethodDelegate<TClass, TResult, TArgs...> >(callee, method, std::move(params));
 }
 
-template <class TClass, typename TResult, typename... TArgs>
+template <class TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::shared_ptr<IDelegate> make_shared_const_method_delegate(
   const TClass* callee, TResult(TClass::* method)(TArgs...) const,  TArgs... args) {
   return std::make_shared<ConstMethodDelegate<TClass, TResult, TArgs...> >(callee, method, DelegateArgs<TArgs...>(std::forward<TArgs>(args)...));
@@ -159,7 +159,7 @@ static std::shared_ptr<IDelegate> make_shared_const_method_delegate(const TClass
   return std::make_shared<ConstMethodDelegate<TClass, TResult, TArgs...> >(callee, method, std::move(params));
 }
 
-template <class TClass, typename TResult, typename... TArgs>
+template <class TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::shared_ptr<IDelegate> make_shared_const_method_delegate(
   std::shared_ptr<TClass> callee, TResult(TClass::* method)(TArgs...) const, TArgs... args) {
   return std::make_shared<SharedConstMethodDelegate<TClass, TResult, TArgs...> >(
@@ -173,7 +173,7 @@ static std::shared_ptr<IDelegate> make_shared_const_method_delegate(std::shared_
 }
 
 
-template <typename TResult, typename... TArgs>
+template <typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::shared_ptr<IDelegate> make_shared_function_delegate(
   TResult f(TArgs...), TArgs... args) {
   return std::make_shared<FunctionalDelegate<TResult, TArgs...> >(
@@ -187,7 +187,7 @@ static std::shared_ptr<IDelegate> make_shared_function_delegate(
     std::function<TResult(TArgs...)>(f), std::move(params));
 }
 
-template <typename TResult, typename... TArgs>
+template <typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::shared_ptr<IDelegate> make_shared_function_delegate(
   std::function<TResult(TArgs...)> func, TArgs... args) {
   return std::make_shared<FunctionalDelegate<TResult, TArgs...> >(func, DelegateArgs<TArgs...>(std::forward<TArgs>(args)...));
@@ -199,7 +199,7 @@ static std::shared_ptr<IDelegate> make_shared_function_delegate(
   return std::make_shared<FunctionalDelegate<TResult, TArgs...> >(func, std::move(params));
 }
 
-template <typename TResult=void, typename... TArgs, typename F>
+template <typename TResult=void, typename... TArgs, typename F, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::shared_ptr<IDelegate> make_shared_lambda_delegate(F && lambda, TArgs... args) {
   return std::make_shared<LambdaDelegate<TResult, F, TArgs...> >(std::move(lambda), DelegateArgs<TArgs...>(std::forward<TArgs>(args)...));
 }
@@ -211,7 +211,7 @@ static std::shared_ptr<IDelegate> make_shared_lambda_delegate(F && lambda, Deleg
 
 // unique
 
-template <class TClass, typename TResult, typename... TArgs>
+template <class TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::unique_ptr<IDelegate> make_unique_method_delegate(
   std::shared_ptr<TClass> callee, TResult (TClass::*method)(TArgs...), TArgs... args) {
   return std::make_unique<SharedMethodDelegate<TClass, TResult, TArgs...> >(
@@ -225,7 +225,7 @@ static std::unique_ptr<IDelegate> make_unique_method_delegate(
     callee, method, std::move(params));
 }
 
-template <class TClass, typename TResult, typename... TArgs>
+template <class TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::unique_ptr<IDelegate> make_unique_method_delegate(
   TClass* callee, TResult (TClass::*method)(TArgs...), TArgs... args) {
   return std::make_unique<MethodDelegate<TClass, TResult, TArgs...> >(callee, method, DelegateArgs<TArgs...>(std::forward<TArgs>(args)...));
@@ -237,7 +237,7 @@ static std::unique_ptr<IDelegate> make_unique_method_delegate(
   return std::make_unique<MethodDelegate<TClass, TResult, TArgs...> >(callee, method, std::move(params));
 }
 
-template <typename TResult, typename... TArgs>
+template <typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::unique_ptr<IDelegate> make_unique_function_delegate(
   TResult f(TArgs...), TArgs... args) {
   return std::make_unique<FunctionalDelegate<TResult, TArgs...> >(
@@ -251,7 +251,7 @@ static std::unique_ptr<IDelegate> make_unique_function_delegate(
     std::function<TResult(TArgs...)>(f), std::move(params));
 }
 
-template <typename TResult, typename... TArgs>
+template <typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::unique_ptr<IDelegate> make_unique_function_delegate(
   std::function<TResult(TArgs...)> func, TArgs... args) {
   return std::make_unique<FunctionalDelegate<TResult, TArgs...> >(func, DelegateArgs<TArgs...>(std::forward<TArgs>(args)...));
@@ -263,7 +263,7 @@ static std::unique_ptr<IDelegate> make_unique_function_delegate(
   return std::make_unique<FunctionalDelegate<TResult, TArgs...> >(func, std::move(params));
 }
 
-template <typename TResult=void, typename... TArgs, typename F>
+template <typename TResult=void, typename... TArgs, typename F, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::unique_ptr<IDelegate> make_unique_lambda_delegate(F && lambda, TArgs... args) {
   return std::make_unique<LambdaDelegate<TResult, F, TArgs...> >(std::move(lambda), DelegateArgs<TArgs...>(std::forward<TArgs>(args)...));
 }
@@ -275,27 +275,27 @@ static std::unique_ptr<IDelegate> make_unique_lambda_delegate(F && lambda, Deleg
 
 // autodetect raw
 
-template <typename TResult, typename... TArgs>
+template <typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static IDelegate* make(TResult f(TArgs...), TArgs... args) {
   return make_function_delegate(f,std::forward<TArgs>(args)...);
 }
 
-template <typename TResult, typename... TArgs>
+template <typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static IDelegate* make(std::function<TResult(TArgs...)> func, TArgs... args) {
   return make_function_delegate(func,std::forward<TArgs>(args)...);
 }
 
-template <typename TClass, typename TResult, typename... TArgs>
+template <typename TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static IDelegate* make(TClass* callee, TResult (TClass::*method)(TArgs...), TArgs... args) {
   return make_method_delegate(callee, method, std::forward<TArgs>(args)...);
 }
 
-template <typename TClass, typename TResult, typename... TArgs>
+template <typename TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static IDelegate* make(std::shared_ptr<TClass> callee, TResult (TClass::*method)(TArgs...), TArgs... args) {
   return make_method_delegate<TClass, TResult, TArgs...>(callee, method, std::forward<TArgs>(args)...);
 }
 
-template <typename TResult=void, typename... TArgs, typename F>
+template <typename TResult=void, typename... TArgs, typename F, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static IDelegate* make(F && lambda, TArgs&&... args) {
   return make_lambda_delegate<TResult, TArgs...>(std::move(lambda), 
     DelegateArgs<TArgs...>(std::forward<TArgs>(args)...));
@@ -306,7 +306,7 @@ static IDelegate* make(F && lambda, DelegateArgs<TArgs...> && params = DelegateA
   return make_lambda_delegate<TResult, TArgs...>(std::move(lambda), std::move(params));
 }
 
-template <typename TResult=void, typename... TArgs>
+template <typename TResult=void, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static ISignal* make_signal(TArgs&&... args) {
   return new detail::SignalBase<TResult, TArgs...>(DelegateArgs<TArgs...>(std::forward<TArgs>(args)...));
 }
@@ -318,22 +318,32 @@ static ISignal* make_signal(DelegateArgs<TArgs...> && params = DelegateArgs<TArg
 
 // autodetect shared
 
-template <typename TResult, typename... TArgs>
+template <typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::shared_ptr<IDelegate> make_shared(TResult f(TArgs...), TArgs... args) {
   return make_shared_function_delegate(f,std::forward<TArgs>(args)...);
 }
 
 template <typename TResult, typename... TArgs>
+static std::shared_ptr<IDelegate> make_shared(TResult f(TArgs...), DelegateArgs<TArgs...>&& params = DelegateArgs<TArgs...>()) {
+  return make_shared_function_delegate(f,std::move(params));
+}
+
+template <typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::shared_ptr<IDelegate> make_shared(std::function<TResult(TArgs...)> func, TArgs... args) {
   return make_shared_function_delegate(func,std::forward<TArgs>(args)...);
 }
 
-template <typename TClass, typename TResult, typename... TArgs>
+template <typename TResult, typename... TArgs>
+static std::shared_ptr<IDelegate> make_shared(std::function<TResult(TArgs...)> func, DelegateArgs<TArgs...>&& params = DelegateArgs<TArgs...>()) {
+  return make_shared_function_delegate(func,std::move(params));
+}
+
+template <typename TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::shared_ptr<IDelegate> make_shared(TClass* callee, TResult (TClass::*method)(TArgs...), TArgs... args) {
   return make_shared_method_delegate(callee, method, std::forward<TArgs>(args)...);
 }
 
-template <typename TClass, typename TResult, typename... TArgs>
+template <typename TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::shared_ptr<IDelegate> make_shared(std::shared_ptr<TClass> callee, TResult (TClass::*method)(TArgs...), TArgs... args) {
   return make_shared_method_delegate<TClass, TResult, TArgs...>(callee, method, std::forward<TArgs>(args)...);
 }
@@ -343,7 +353,7 @@ static std::shared_ptr<IDelegate> make_shared(std::shared_ptr<TClass> callee, TR
   return make_shared_method_delegate<TClass, TResult, TArgs...>(callee, method, std::move(params));
 }
 
-template <typename TClass, typename TResult, typename... TArgs>
+template <typename TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::shared_ptr<IDelegate> make_shared(const TClass* callee, TResult(TClass::* method)(TArgs...) const, TArgs... args) {
   return make_shared_const_method_delegate<TClass, TResult, TArgs...>(callee, method, std::forward<TArgs>(args)...);
 }
@@ -353,7 +363,7 @@ static std::shared_ptr<IDelegate> make_shared(const TClass* callee, TResult(TCla
   return make_shared_const_method_delegate<TClass, TResult, TArgs...>(callee, method, std::move(params));
 }
 
-template <typename TClass, typename TResult, typename... TArgs>
+template <typename TClass, typename TResult, typename... TArgs, bool TCheck=true, typename = typename std::enable_if<sizeof...(TArgs) && TCheck>::type>
 static std::shared_ptr<IDelegate> make_shared(std::shared_ptr<TClass> callee, TResult(TClass::* method)(TArgs...) const, TArgs... args) {
   return make_shared_const_method_delegate<TClass, TResult, TArgs...>(callee, method, std::forward<TArgs>(args)...);
 }
